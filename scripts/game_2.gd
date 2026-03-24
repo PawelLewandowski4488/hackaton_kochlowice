dee9548cdf826177ed45589249ca6cdb3ad6cfc9
sadson keenolee <pawell4488@gmail.com>
2026-03-24 18:23:28 +0100

    'play2'

---
+++ scripts/game_2.gd
@@ -0,0 +1,74 @@
+extends Node
+
+var objects_path = "res://scenes/objects/"
+
+@onready var ice_cube = $IceCube
+@onready var col_up = $Room/CollisionShape_Up
+@onready var col_right = $Room/CollisionShape_Right
+@onready var col_front = $Room/CollisionShape_Front
+@onready var mesh = $Room/MeshInstance3D
+@onready var light = $Room/OmniLight3D
+
+func _ready():
+	load_level_from_json("user://maps/test.json");
+	
+func update_level_size():
+	col_right.position.x = GlobalData.level_size[0]
+	col_up.position.y = GlobalData.level_size[1]
+	col_front.position.z = GlobalData.level_size[2]
+	mesh.position = GlobalData.level_size / 2
+	mesh.mesh.size = GlobalData.level_size
+	light.position = GlobalData.level_size / 2
+	light.omni_range = max(GlobalData.level_size[0], GlobalData.level_size[1], GlobalData.level_size[2])
+	
+
+func load_level_from_json(path: String):
+	if not FileAccess.file_exists(path):
+		print("Błąd: Plik nie istnieje!")
+		return
+
+	var file = FileAccess.open(path, FileAccess.READ)
+	var json_string = file.get_as_text()
+	file.close()
+
+	var json = JSON.new()
+	var error = json.parse(json_string)
+	if error != OK:
+		print("Błąd parsowania JSON: ", json.get_error_message())
+		return
+
+	var data = json.data
+	
+	if data.has("level_size"):
+		var sz = data["level_size"]
+		GlobalData.level_size = Vector3i(sz.x, sz.y, sz.z)
+		update_level_size()
+	
+	for obj_data in data["objects"]:
+		var type = obj_data["type"] 
+		
+		if type == 'start_position':
+			var pos = obj_data["pos"]
+			var rot = obj_data["rot"]
+			ice_cube.global_position = Vector3(pos.x, pos.y, pos.z)
+			ice_cube.global_rotation_degrees = Vector3(rot.x, rot.y, rot.z)
+		else:
+			var full_path = objects_path + type + ".tscn"
+			var scene = load(full_path)
+			
+			if scene:
+				var instance = scene.instantiate()
+				
+				#instance.set_meta("object_id", type)
+				#instance.add_to_group("built_objects")
+				add_child(instance)
+				
+				var pos = obj_data["pos"]
+				var rot = obj_data["rot"]
+				var scl = obj_data["scale"]
+				
+				instance.global_position = Vector3(pos.x, pos.y, pos.z)
+				instance.global_rotation_degrees = Vector3(rot.x, rot.y, rot.z)
+				instance.scale = Vector3(scl.x, scl.y, scl.z)
+			else:
+				print("Nie udało się załadować sceny: ", full_path)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 const GAME_SCENE_PATH = "res://scenes/game.tscn" 
 const LEVEL_MANAGER_SCENE_PATH = "res://scenes/level_manager.tscn"
+const GAME_2_SCENE_PATH = "res://scenes/game_2.tscn" 
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -14,3 +15,7 @@ func _on_quit_pressed() -> void:
 	
 func _on_level_editor_pressed() -> void:
 	get_tree().change_scene_to_file(LEVEL_MANAGER_SCENE_PATH)
+
+
+func _on_play_2_pressed() -> void:
+	get_tree().change_scene_to_file(GAME_2_SCENE_PATH)
